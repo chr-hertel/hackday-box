7ad3f14a60775b0a96db7b97b7b9c030ff193397
Christopher Hertel <mail@christopher-hertel.de>
2016-03-19 15:35:31 +0100

    added phar demo app

---
+++ src/php5/phar-demo/src/Command/DemoCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PharDemo\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DemoCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('demo')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello world!');
+    }
+
+}
