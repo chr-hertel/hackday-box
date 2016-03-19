d95f74f71b889824d6bd2cd061b08fff472d0fe5
Christopher Hertel <mail@christopher-hertel.de>
2016-03-19 12:33:35 +0100

    added php5 return value test

---
+++ src/php5/type_test/test.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Returning a string:".PHP_EOL;
+var_dump(return_string());
+
+echo "Returning an integer:".PHP_EOL;
+var_dump(return_int());
+
+echo "Returning a float:".PHP_EOL;
+var_dump(return_float());
+
+echo "Returning a boolean:".PHP_EOL;
+var_dump(return_bool());
+
+echo "Returning null:".PHP_EOL;
+var_dump(return_null());
+
+echo "Returning an array:".PHP_EOL;
+var_dump(return_array());
